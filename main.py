3964615aec6f445eb9bb3724573db1b8d4a9c1f7
Frank McCrudden <fmccrudden@cps.edu>
2025-10-10 10:14:43 -0500

    started list

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-https://documenter.getpostman.com/view/11586746/SztEa7bL
+#https://documenter.getpostman.com/view/11586746/SztEa7bL
 
-http://ergast.com/api/f1/drivers?=123
+import requests
+api_url = "http://ergast.com/api/f1/drivers?=123"
+response = requests.get(api_url)
+
+response.raise_for_status()
+
+data = response.json()
+
+print(data)
+
+f1driverdb: List[str,str,str, List[str]] = [
+    (
+        "Max Verstappen"
+        "Netherlands"
+        [
+           "Red Bull"
+           "EA Sports"
+           "Heiken" 
+        ]
+    )
+]
