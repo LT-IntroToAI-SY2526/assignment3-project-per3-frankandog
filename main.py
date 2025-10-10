ebb2e7ae7e3b161078632dc59cbbecfc820c4f67
Frank McCrudden <fmccrudden@cps.edu>
2025-10-10 10:35:15 -0500

    2/3 done of database

--- main.py
+++ main.py
@@ -18,6 +18,102 @@ f1driverdb: List[str,str,str, List[str]] = [
            "Red Bull"
            "EA Sports"
            "Heiken" 
+        ],
+    ),
+    (
+        "Lewis Hamilton" 
+        "United Kingdom"
+        [
+            "Tommy Hilfiger"
+            "Puma"
+            "Sony"
+            "Lululemon"
+        ],
+    ),
+    (
+        "Charles Leclerc"
+        "Monaco"
+        [
+            "Richard Mille"
+            "PUMA"
+            "APM Monaco"
+            "VistaJet"
+        ],
+    ),
+    (
+        "Fernando Alonso"
+        "Spain"
+        [
+           "Aramco" 
+        ],
+    ),
+    (
+        "Lando Norris"
+        "United Kingdom"
+        [
+            "McLaren’s partner brands"
+            "Mastercard"
+            "Google"
+        ],
+    ),
+    (
+        "Oscar Piastri"
+        "Australia"
+        [
+            "Dubber"
+            "Quad Lock"
+            "HP Tuners"
+        ],
+    ),
+    (
+        "Pierre Gasly"
+        "France"
+        [
+            "Givenchy Parfums"
+            "Binance"
+            "Fantom"
+            "AIX Investment Group"
+        ],
+    ),
+    (
+        "Esteban Ocon"
+        "France"
+        [
+            "Alpine"
+        ],
+    ),
+    (
+        "George Russell"
+        "United Kingdom"
+        [
+            "Mercedes"
+        ],
+    ),
+    (
+        "Lance Stroll"
+        "Canada"
+        [
+            "Aston Martin"
+        ],
+    ),
+    (
+        "Yuki Tsunoda"
+        "Japan"
+        [
+            "Visa"
+            "Honda"
+            "Redbull"
+            "HexClad"
+        ],
+    ),
+    (
+        "Isack Hadjar"
+        "France"
+        [
+           "Redbull"
+           "Visa"
+           "Cashapp"
+           "Airtasker" 
         ]
     )
 ]
