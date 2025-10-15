947f28c78403c9a1b268cb068106c8232d6a6233
Sanyah Bovan <sbovan1@cps.edu>
2025-10-15 10:29:48 -0500

    commas and request

--- main.py
+++ main.py
@@ -1,119 +1,120 @@
 #https://documenter.getpostman.com/view/11586746/SztEa7bL
+from typing import List, Tuple, Callable, Any
 
-import requests
-api_url = "http://ergast.com/api/f1/drivers?=123"
-response = requests.get(api_url)
+# import requests
+# api_url = "http://ergast.com/api/f1/drivers?=123"
+# response = requests.get(api_url)
 
-response.raise_for_status()
+# response.raise_for_status()
 
-data = response.json()
+# data = response.json()
 
-print(data)
+# print(data)
 
 f1driverdb: List[str,str,str, List[str]] = [
     (
-        "Max Verstappen"
-        "Netherlands"
+        "Max Verstappen",
+        "Netherlands",
         [
-           "Red Bull"
-           "EA Sports"
-           "Heiken" 
+           "Red Bull",
+           "EA Sports",
+           "Heiken",
         ],
     ),
     (
-        "Lewis Hamilton" 
-        "United Kingdom"
+        "Lewis Hamilton", 
+        "United Kingdom",
         [
-            "Tommy Hilfiger"
-            "Puma"
-            "Sony"
-            "Lululemon"
+            "Tommy Hilfiger",
+            "Puma",
+            "Sony",
+            "Lululemon",
         ],
     ),
     (
-        "Charles Leclerc"
-        "Monaco"
+        "Charles Leclerc",
+        "Monaco",
         [
-            "Richard Mille"
-            "PUMA"
-            "APM Monaco"
-            "VistaJet"
+            "Richard Mille",
+            "PUMA",
+            "APM Monaco",
+            "VistaJet",
         ],
     ),
     (
-        "Fernando Alonso"
-        "Spain"
+        "Fernando Alonso",
+        "Spain",
         [
-           "Aramco" 
+           "Aramco", 
         ],
     ),
     (
-        "Lando Norris"
-        "United Kingdom"
+        "Lando Norris",
+        "United Kingdom",
         [
-            "McLaren’s partner brands"
-            "Mastercard"
-            "Google"
+            "McLaren’s partner brands",
+            "Mastercard",
+            "Google",
         ],
     ),
     (
-        "Oscar Piastri"
-        "Australia"
+        "Oscar Piastri",
+        "Australia",
         [
-            "Dubber"
-            "Quad Lock"
-            "HP Tuners"
+            "Dubber",
+            "Quad Lock",
+            "HP Tuners",
         ],
     ),
     (
-        "Pierre Gasly"
-        "France"
+        "Pierre Gasly",
+        "France",
         [
-            "Givenchy Parfums"
-            "Binance"
-            "Fantom"
-            "AIX Investment Group"
+            "Givenchy Parfums",
+            "Binance",
+            "Fantom",
+            "AIX Investment Group",
         ],
     ),
     (
-        "Esteban Ocon"
-        "France"
+        "Esteban Ocon",
+        "France",
         [
-            "Alpine"
+            "Alpine",
         ],
     ),
     (
-        "George Russell"
-        "United Kingdom"
+        "George Russell",
+        "United Kingdom",
         [
-            "Mercedes"
+            "Mercedes",
         ],
     ),
     (
-        "Lance Stroll"
-        "Canada"
+        "Lance Stroll",
+        "Canada",
         [
-            "Aston Martin"
+            "Aston Martin",
         ],
     ),
     (
-        "Yuki Tsunoda"
-        "Japan"
+        "Yuki Tsunoda",
+        "Japan",
         [
-            "Visa"
-            "Honda"
-            "Redbull"
-            "HexClad"
+            "Visa",
+            "Honda",
+            "Redbull",
+            "HexClad",
         ],
     ),
     (
-        "Isack Hadjar"
-        "France"
+        "Isack Hadjar",
+        "France",
         [
-           "Redbull"
-           "Visa"
-           "Cashapp"
-           "Airtasker" 
-        ]
-    )
+           "Redbull",
+           "Visa",
+           "Cashapp",
+           "Airtasker", 
+        ],
+    ),
 ]
